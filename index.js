fd94d90d110cc2a598ab2f817814317b743b3e12
rajZadafiya55 <zadagfiyaraj395.com>
2025-04-23 20:21:03 +0530

    server completed final

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import passport from 'passport';
-import session from 'express-session';
-import authRoutes from './routes/authRoutes.js';
-import resumeRoutes from './routes/resumeRoutes.js';
-import connectDB from './config/db.js';
-import './config/passport.js';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+import passport from "passport";
+import session from "express-session";
+import authRoutes from "./routes/authRoutes.js";
+import resumeRoutes from "./routes/resumeRoutes.js";
+import connectDB from "./config/db.js";
+import "./config/passport.js";
 
 dotenv.config();
 connectDB();
@@ -27,24 +27,24 @@ const app = express();
 //   credentials: true,
 // }));
 
-app.use(cors({origin:true,credentials:true}));
-app.options('*', cors());
+app.use(cors({ origin: "*", credentials: true }));
+app.options("*", cors());
 // app.use(cors({ origin: "*", credentials: true }));
 app.use(express.json());
 
 app.use(
   session({
-    secret: 'secret',
+    secret: "secret",
     resave: false,
-    saveUninitialized: true
+    saveUninitialized: true,
   })
 );
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use('/api/auth', authRoutes);
-app.use('/api/resume', resumeRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/resume", resumeRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
